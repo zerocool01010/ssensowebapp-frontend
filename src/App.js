716b104e0b2179b7617fbce23eb1d647caaeb044
Florencia Fernandez <florenciafernandez0301@gmail.com>
2024-02-18 00:06:58 -0300

    created header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from './header/header.js';
+
 function App() {
   return (
     <div>
-      Samurai Senso Web App is coming!
+      <Header />
+      <div>
+        Samurai Senso Web App is coming!
+      </div>
     </div>
   );
 }
