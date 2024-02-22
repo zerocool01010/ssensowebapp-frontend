9cde85dcc2ea458f6d035a386109cf56199107ff
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-02-22 14:58:59 -0300

    navbar interactive and border bottom color

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,27 +6,21 @@ const Header = () => {
 
   const location = useLocation()
   const currentPath = location.pathname
+  let currentPathSliced = currentPath.slice(1)
+  if (currentPathSliced === '') {
+    currentPathSliced = 'prefecturas'
+  }
 
   const navBarItems = ['prefecturas', 'clanes', 'historia', 'personajes']
 
   const navBarHTMLElement = navBarItems.map((item) => (
-    <Link>
-      <div className={`text-lg capitalize font-inkAnt ${item === currentPath ? 'text-[#F7DBA1]' : 'text-black'}`}>{item}</div>
+    <Link to={`/${item}`}>
+      <div className={`text-lg capitalize font-inkAnt ${item === currentPathSliced ? 'text-[#F7DBA1]' : 'text-black'}`}>{item}</div>
     </Link>
   ))
-
-  /* extraer el path location con useLocation */
-  /* extraer solo el nombre final de la url con un slice, de modo que nos quede el string limpio. Guardar esto en una variable X */
-  /* crear un arreglo F de tipo string con los nombres de los elementos del navbar */
-  /* hacer un bucle para crear dinamicamente los elementos del navbar:
-      - recorrer el arreglo F y por cada iteracion crear un elemento con la forma: <Link>
-            <div className='text-black text-lg capitalize font-inkAnt'>{elemName}</div>
-          </Link>
-      - cuando se cumpla la condicion X === F agregar una clase tailwind para estilizar y pintar este elemento del navbar con el color correspondiente    
-          */
-
+  
   return <>
-    <header className="w-full bg-[#F7DBA1] flex flex-col items-center p-1">
+    <header className="w-full bg-[#DEC38A] flex flex-col items-center p-1 border border-b-4 border-[#F7DBA1]">
       <Link to="/">  {/* Aca estoy ruteando a App, pero quiero hacerlo a Home después */}
         <img src={logo} className='w-[109px] h-[50px]' alt="Logo" />
       </Link>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Prefecturas from './pages/Prefecturas';
+import { Clanes } from './pages/Clanes';
+import { Historia } from './pages/Historia';
+import { Personajes } from './pages/Personajes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +14,10 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Prefecturas />} />
+        <Route path="/prefecturas" element={<Prefecturas />} />
+        <Route path="/clanes" element={<Clanes />} />
+        <Route path="/historia" element={<Historia />} />
+        <Route path="/personajes" element={<Personajes />} />
         <Route path="*" element={<h1>Route does not
           exist</h1>} />
       </Routes>
