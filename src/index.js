ed15313b07b99a0eebe741c292527f284a2cf4c6
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-02-22 13:48:44 -0300

    layout called and Prefecturas.js file created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Prefecturas from './pages/Prefecturas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Prefecturas />} />
         <Route path="*" element={<h1>Route does not
           exist</h1>} />
       </Routes>
---
+++ src/pages/Prefecturas.js
@@ -0,0 +1,15 @@
+import { Layout } from "../components/layout/layout";
+
+const Prefecturas = () => {
+  return (
+    <div>
+      <Layout>
+        <div>
+          Samurai Senso Web App is coming!
+        </div>
+      </Layout>
+    </div>
+  );
+}
+
+export default Prefecturas;
